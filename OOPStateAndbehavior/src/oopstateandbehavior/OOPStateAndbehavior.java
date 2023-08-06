f326b6bf3d0f1d396c131b63c8343d9099fa5818
Masud Rana <masudrana.20091@gmail.com>
2023-08-06 13:31:14 +0600

    ADD

--- OOPStateAndbehavior/src/oopstateandbehavior/OOPStateAndbehavior.java
+++ OOPStateAndbehavior/src/oopstateandbehavior/OOPStateAndbehavior.java
@@ -11,7 +11,7 @@ public class OOPStateAndbehavior {
     public static void main(String[] args) {
         Circle c=new Circle();
         c.radius=5;
-        
+         
         System.out.println("Area: "+c.getArea());
         System.out.println("Perimeter: "+c.gePerimeter());
         
