95ed2d12ea32a46159ef097e262cf2cf25212a06
Masud Rana <masudrana.20091@gmail.com>
2023-08-24 13:06:32 +0600

    add

---
+++ FactorialInRecurtion/src/factorialinrecurtion/FactorialInRecurtion.java
@@ -0,0 +1,29 @@
+
+package factorialinrecurtion;
+
+
+public class FactorialInRecurtion {
+
+
+    public static void main(String[] args) {
+        
+        Factorial fc=new Factorial
+    }
+    
+    
+    
+    
+    
+    public static void main(int[] fact) {
+        
+        int fact (int n){
+        
+        if(n==1)
+            return 1;
+        
+        else
+           return n*fact(n-1);
+        
+    }
+    }
+}
