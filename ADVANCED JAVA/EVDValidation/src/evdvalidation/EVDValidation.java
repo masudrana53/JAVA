c74e37a7aa328885da9fdfb25b73851917785566
Masud Rana <masudrana.20091@gmail.com>
2023-09-10 13:23:38 +0600

    Add

---
+++ ADVANCED JAVA/EVDValidation/src/evdvalidation/EVDValidation.java
@@ -0,0 +1,12 @@
+
+package evdvalidation;
+
+
+public class EVDValidation {
+
+   
+    public static void main(String[] args) {
+       
+    }
+    
+}
---
+++ ADVANCED JAVA/PracticeDbCon/src/practicedbcon/PracticeDbCon.java
@@ -0,0 +1,13 @@
+
+package practicedbcon;
+
+
+public class PracticeDbCon {
+
+   
+    public static void main(String[] args) {
+        
+        
+    }
+    
+}
