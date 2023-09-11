985950d9bcd121d5b87c7b1bbbb11661c56834a8
Masud Rana <masudrana.20091@gmail.com>
2023-09-11 13:09:24 +0600

    Add

---
+++ ADVANCED JAVA/Evidence Test/StudentFrom/src/studentfrom/StudentFrom.java
@@ -0,0 +1,17 @@
+
+package studentfrom;
+
+import view.StudentView;
+
+
+public class StudentFrom {
+
+
+    public static void main(String[] args) {
+       
+        StudentView sv=new StudentView();
+        sv.setLocationRelativeTo(null);
+        sv.setVisible(true);
+    }
+    
+}
