1aca0d75fe4012cb94d87a5909eef7d4b82dafea
Masud Rana <masudrana.20091@gmail.com>
2023-11-19 13:12:01 +0600

    add

--- Angular/Code/Angularthird/src/app/app.component.ts
+++ Angular/Code/Angularthird/src/app/app.component.ts
@@ -1,18 +1,14 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
 import { TodoList } from './todolist';
 import { TodoItem } from './todoitem';
 
-
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+
   private list = new TodoList("List",[
     new TodoItem("Singra Khabo"),
     new TodoItem("Joriman Korbo"),
@@ -28,6 +24,5 @@ export class AppComponent {
     return this.list.items.filter(item => !item.complete).length;
   }
 
-
-  title = 'MyAngular';
+  title = 'Angularthird';
 }
