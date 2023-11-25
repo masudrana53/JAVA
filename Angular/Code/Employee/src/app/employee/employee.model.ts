47f579a5ed6bc874a7d00360a2f23453fc937215
Masud Rana <masudrana.20091@gmail.com>
2023-11-25 13:08:06 +0600

    add

---
+++ Angular/Code/Employee/src/app/employee/employee.model.ts
@@ -0,0 +1,7 @@
+export class EmployeeModel{
+    id: number=0;
+        name:string='';
+        salary:string='';
+        gender:string='';
+        hobby:string='';
+}
---
+++ Angular/Code/Employee/src/app/services/employee.service.ts
@@ -0,0 +1,11 @@
+import { Injectable, OnInit } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { map } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+
+  constructor() { }
+}
